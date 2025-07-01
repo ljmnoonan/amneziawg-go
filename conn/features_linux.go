cb677686a3433d4c8af1f1a54362f23d839d1959
ljmnoonan <ljmnoonan@gmail.com>
2025-06-30 23:42:55 -0400

    Trying to fix the build error

--- conn/features_linux.go
+++ conn/features_linux.go
@@ -7,15 +7,9 @@ package conn
 
 import (
 	"net"
-
-	"golang.org/x/sys/unix"
 )
 
 func supportsUDPOffload(conn *net.UDPConn) (txOffload, rxOffload bool) {
-	rc, err := conn.SyscallConn()
-	if err != nil {
-		return false, false
-	}
 	// There appears to be an architecture-specific bug on arm64 related to
 	// UDP GSO/GRO that causes data packets to be misinterpreted after a
 	// successful handshake. Forcing this feature off for all builds ensures
